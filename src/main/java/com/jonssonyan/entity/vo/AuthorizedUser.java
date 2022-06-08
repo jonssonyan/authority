b60afc7650bbdc14cec08798bc7308bfa52b4cef
jonssonyan <yz808@outlook.com>
2022-06-08 10:25:13 +0800

    fix

--- src/main/java/com/jonssonyan/entity/vo/AuthorizedUser.java
+++ src/main/java/com/jonssonyan/entity/vo/AuthorizedUser.java
@@ -2,8 +2,11 @@ package com.jonssonyan.entity.vo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class AuthorizedUser {
+public class AuthorizedUser implements Serializable {
+    private static final long serialVersionUID = -8152270249992675066L;
     private Long id;
     private String username;
     private Long roleId;
